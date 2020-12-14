7a02f254f0d2180ce22facb376d5b54228ad64eb
bunnie <bunnie@kosagi.com>
2020-12-15 00:46:49 +0800

    update with changes to reflect Xous server implementation

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> Result<(), BridgeError> {
     // connects to a device with the product ID of 0x5bf0.
     let bridge = UsbBridge::new().pid(0x5bf0).create()?;
 
-    let ram_a = 0x4008_0000;
+    let ram_a = 0x4080_0000;
     let burst_len = 512 * 1024;
 
     /*
@@ -22,26 +22,27 @@ fn main() -> Result<(), BridgeError> {
         handle.write_all(&page)?;
     }*/
 
-    let ram_b = 0x4010_0000;
-    let messible2_in = 0xf001_0000; // replace with messible2_in
-    let messible_out = 0xf000_f004;
+    let ram_b = 0x4090_0000;
+    let messible2_in = 0xf001_1000; // replace with messible2_in
+    let messible_out = 0xf001_0004;
 
     let mut phase = 0x1;
-    bridge.poke(messible2_in, 1)?;
-    bridge.poke(messible2_in, 2)?;
+    bridge.poke(messible2_in, 2)?;  // start with a fill request for the next phase, so we can meet the lockstep criteria
     loop {
-        // wait for a new phase
+        // wait for a new phase -- don't check the "have", just read the fifo, b/c USB packets are expensive
         while phase != bridge.peek(messible_out)? {
-            thread::sleep(time::Duration::from_millis(10));
+            thread::sleep(time::Duration::from_millis(1));
             //eprintln!("waiting for {}, got {} ", phase, bridge.peek(messible_out)?);
         }
         thread::sleep(time::Duration::from_millis(5));
 
         // dispatch on phase
         if phase == 1 {
-            bridge.poke(messible2_in, 2)?; // sending 2 fills B
-            // so read A
-            let page = match bridge.burst_read(ram_a, burst_len) {
+	    // in this case, we must have requested B buffer to fill, because we got a B (that is, 2) ACK
+            bridge.poke(messible2_in, 1)?; // sending 1 request concurrent fill of A while we read B
+	    
+            // read B concurrently with A fill
+            let page = match bridge.burst_read(ram_b, burst_len) {
                 Err(e) => {
                     eprintln!("USB bridge error {}, ignoring packet (phase 1)", e);
                     vec![] // just skip this data and return the next packet
@@ -55,9 +56,9 @@ fn main() -> Result<(), BridgeError> {
             //eprintln!("read {} bytes", page.len());
             handle.write_all(&page)?;
         } else {
-            bridge.poke(messible2_in, 1)?; // sending 1 fills A
-            // so read B
-            let page = match bridge.burst_read(ram_b, burst_len) {
+            bridge.poke(messible2_in, 2)?; // sending 2 concurrently fills B
+            // read A while B fills
+            let page = match bridge.burst_read(ram_a, burst_len) {
                 Err(e) => {
                     eprintln!("USB bridge error {}, ignoring packet (phase 2)", e);
                     vec![]
